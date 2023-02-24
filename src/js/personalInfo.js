91410ee65cdadf76e201951c9661491f69cc0619
cynthiawang72 <>
2023-02-24 13:15:39 +0800

    個人基本資料 新增學校類型、學校所在地檢查條件

--- src/js/personalInfo.js
+++ src/js/personalInfo.js
@@ -927,8 +927,10 @@
 
         // 學歷
         _checkValue($educationSystemDescription,'General','學制描述',true);
-        _checkValue($schoolNameSelect,'General','學校名稱',true);
-        _checkValue($schoolNameText,'General','其他學校名稱',!_hasSchoolLocate);
+        _checkValue($schoolNameSelect,'General','學校名稱',_hasSchoolLocate);
+        _checkValue($schoolNameText,'General','學校名稱',!_hasSchoolLocate);
+        _checkValue($schoolType,'else','學校類別',_hasEduType);
+        _checkValue($schoolLocation,'else','學校所在地',_hasEduType);
         _checkValue($schoolAdmissionAt,'General','入學時間',true);
         _checkValue($schoolGraduateAt,'General','畢業時間',true);
 
@@ -1003,8 +1005,8 @@
                 taiwan_address: ($taiwanAddress === null)? "" : $taiwanAddress.val(),
                 education_system_description: $educationSystemDescription.val(),
                 school_country: $schoolCountry.val(),
-                school_type: $schoolType.val(),
-                school_locate: $schoolLocation.val(),
+                school_type: (_hasEduType)? $schoolType.val():'',
+                school_locate: (_hasEduType)? $schoolLocation.val():'',
                 school_name: (_hasSchoolLocate)? $schoolNameSelect.val() : $schoolNameText.val(),
                 school_admission_at: $schoolAdmissionAt.val(),
                 school_graduate_at: $schoolGraduateAt.val(),
